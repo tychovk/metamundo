4427188d05e17ec1dcdf26a90a306f9d0fbad77a
tychovk <tychovk@gmail.com>
2016-06-11 20:53:04 -0500

    renamed folders, everything works still!

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-import celery
-import config
-import redis #necessary???
-
-cel_app = celery.Celery('tasks', broker=config.celery_config.CELERY_BROKER_URL)
-cel_app.config_from_object(config.celery_config)
-
