ccd2da67afa1d4047418b198f7bcf9d3f7e7f97b
tychovk <tychovk@gmail.com>
2016-06-22 18:41:45 -0500

    css stuff

--- metamundo/settings.py
+++ metamundo/settings.py
@@ -127,12 +127,11 @@ USE_TZ = True
 
 
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static'))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 
