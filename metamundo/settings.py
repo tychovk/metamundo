3a41597391381aedad8bf82a37a2f0a4d6b0f8e3
tychovk <tychovk@gmail.com>
2016-06-22 18:23:45 -0500

    css tries

--- metamundo/settings.py
+++ metamundo/settings.py
@@ -125,13 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static'))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
-
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 import dj_database_url
