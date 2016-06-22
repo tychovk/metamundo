12cdf10ca5f3bac4af0feabb1e0c2ccc03f7b77c
tychovk <tychovk@gmail.com>
2016-06-22 11:04:35 -0500

    staticroot

--- metamundo/metamundo/settings.py
+++ metamundo/metamundo/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
